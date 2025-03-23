1a45000a4bfcd0beed7c0d544fe589736bcb5a3b
sraleao <ellen20240025960@alu.uern.br>
2025-03-23 15:41:55 -0300

    Criação da API RESTful com rotas básicas

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 # Criar uma instancia do flask
 app = Flask(__name__)
 
-# Dados
+# Lista de filmes
 filmes = [
   {'id': 1, 'titulo': 'A proposta', 'descricao': 'Uma editora de livros canadense que força seu assistente a se casar com ela para evitar a deportação, e eles acabam se apaixonando.'},
   {'id': 2, 'titulo': 'Dez Coisas que Odeio em Voce', 'descricao': 'Um garoto é pago para namorar a irmã rebelde de uma garota popular, mas acaba se apaixonando por ela de verdade.'},
